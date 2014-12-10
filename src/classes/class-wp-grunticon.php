b4a199386ee78de71c4e5a640d69f00084bd4d4c
Michael Novotny <manovotny@gmail.com>
2014-12-10 16:52:20 -0600

    Replaces HTML output string concatination with `echo`.

--- src/classes/class-wp-grunticon.php
+++ src/classes/class-wp-grunticon.php
@@ -70,26 +70,23 @@ class WP_Grunticon {
 
         if ( ! empty( $queue ) ) {
 
-            $html = '';
             $noscript = array();
 
-            $html .= '<script type="text/javascript">';
-                $html .= 'window.grunticon=function(e){if(e&&3===e.length){var t=window,n=!(!t.document.createElementNS||!t.document.createElementNS("http://www.w3.org/2000/svg","svg").createSVGRect||!document.implementation.hasFeature("http://www.w3.org/TR/SVG11/feature#Image","1.1")||window.opera&&-1===navigator.userAgent.indexOf("Chrome")),o=function(o){var r=t.document.createElement("link"),a=t.document.getElementsByTagName("script")[0];r.rel="stylesheet",r.href=e[o&&n?0:o?1:2],a.parentNode.insertBefore(r,a)},r=new t.Image;r.onerror=function(){o(!1)},r.onload=function(){o(1===r.width&&1===r.height)},r.src="data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///ywAAAAAAQABAAACAUwAOw=="}};';
+            echo '<script type="text/javascript">';
+                echo 'window.grunticon=function(e){if(e&&3===e.length){var t=window,n=!(!t.document.createElementNS||!t.document.createElementNS("http://www.w3.org/2000/svg","svg").createSVGRect||!document.implementation.hasFeature("http://www.w3.org/TR/SVG11/feature#Image","1.1")||window.opera&&-1===navigator.userAgent.indexOf("Chrome")),o=function(o){var r=t.document.createElement("link"),a=t.document.getElementsByTagName("script")[0];r.rel="stylesheet",r.href=e[o&&n?0:o?1:2],a.parentNode.insertBefore(r,a)},r=new t.Image;r.onerror=function(){o(!1)},r.onload=function(){o(1===r.width&&1===r.height)},r.src="data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///ywAAAAAAQABAAACAUwAOw=="}};';
 
                 foreach ( $queue as $wp_grunticon_options ) {
 
-                    $html .= $wp_grunticon_options->generate_javascript_markup();
+                    echo $wp_grunticon_options->generate_javascript_markup();
 
                     array_push( $noscript, $wp_grunticon_options->generate_noscript_markup() );
 
                 }
 
-            $html .= '</script>';
-            $html .= '<noscript>';
-                $html .= implode( PHP_EOL, $noscript );
-            $html .= '</noscript>';
-
-            echo $html;
+            echo '</script>';
+            echo '<noscript>';
+                echo implode( PHP_EOL, $noscript );
+            echo '</noscript>';
 
         }
 
